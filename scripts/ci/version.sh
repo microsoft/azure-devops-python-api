1b1a9a891ae5d4be3b64ddb9c048d43993aea592
Ted Chambers <tedchamb@microsoft.com>
2017-10-23 14:13:18 -0400

    add bash script to update version

---
+++ scripts/ci/version.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# Update the version strings in the source code
+
+# Input:
+#   $1 - the version string, if omitted, use ${!BUILD_BUILDID}
+
+version=$1
+
+if [ -z ${version} ]; then
+    version=${!BUILD_BUILDID}
+fi
+
+if [ -z ${version} ]; then
+    echo 'Missing version string'
+    exit 1
+fi
+
+echo "Replace with version: $version"
+
+platform=`uname`
+
+for each in $(find . -name setup.py); do
+    if [ "$platform" == "Darwin" ]; then
+        sed -i "" "s/^VERSION = [\"']\(.*\)[\"']/VERSION = \"\1.dev$version\"/" ${each}
+    else
+        sed -i "s/^VERSION = [\"']\(.*\)[\"']/VERSION = \"\1.dev$version\"/" ${each}
+    fi
+done
+
+for each in $(find . -name version.py); do
+    if [ "$platform" == "Darwin" ]; then
+        sed -i "" "s/^VERSION = [\"']\(.*\)[\"']/VERSION = \"\1.dev$version\"/" ${each}
+    else
+        sed -i "s/^VERSION = [\"']\(.*\)[\"']/VERSION = \"\1.dev$version\"/" ${each}
+    fi
+done
