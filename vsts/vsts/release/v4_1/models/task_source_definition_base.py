bdb3f73b58d1974e5c280e03e17e3f8fe485604c
Ted Chambers <tedchamb@microsoft.com>
2018-04-25 20:26:27 -0400

    fix issue where BaseSecuredObject was incorrectly set as base class for some models.

--- vsts/vsts/release/v4_1/models/task_source_definition_base.py
+++ vsts/vsts/release/v4_1/models/task_source_definition_base.py
@@ -6,9 +6,10 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------------------------
 
+from msrest.serialization import Model
 
 
-class TaskSourceDefinitionBase(BaseSecuredObject):
+class TaskSourceDefinitionBase(Model):
     """TaskSourceDefinitionBase.
 
     :param auth_key:
--- vsts/vsts/work/v4_1/models/work_item_tracking_resource_reference.py
+++ vsts/vsts/work/v4_1/models/work_item_tracking_resource_reference.py
@@ -6,9 +6,10 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------------------------
 
+from msrest.serialization import Model
 
 
-class WorkItemTrackingResourceReference(BaseSecuredObject):
+class WorkItemTrackingResourceReference(Model):
     """WorkItemTrackingResourceReference.
 
     :param url:
