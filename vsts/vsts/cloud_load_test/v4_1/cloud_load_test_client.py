00ecf7aceccd6ba59c392c5f6f51e2049d2acc87
Ted Chambers <tedchamb@microsoft.com>
2018-07-13 18:17:23 -0400

    Adding more resource area ids.

--- vsts/vsts/cloud_load_test/v4_1/cloud_load_test_client.py
+++ vsts/vsts/cloud_load_test/v4_1/cloud_load_test_client.py
@@ -23,7 +23,7 @@ class CloudLoadTestClient(VssClient):
         self._serialize = Serializer(client_models)
         self._deserialize = Deserializer(client_models)
 
-    resource_area_identifier = None
+    resource_area_identifier = '7ae6d0a6-cda5-44cf-a261-28c392bed25c'
 
     def create_agent_group(self, group):
         """CreateAgentGroup.
