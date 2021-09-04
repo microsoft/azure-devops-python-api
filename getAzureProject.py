8415ebda0dba7596350a2814c69728c4ba9b2c5b
tpwange <tpwange@gmail.com>
2021-09-04 21:37:49 +0800

    20210904-2137

--- getAzureProject.py
+++ getAzureProject.py
@@ -1,20 +1,64 @@
 import os
 import requests
+import json
 
 # See link down below to generate your Private Access Token
 personal_access_token = '42muadbwmvihps424suvtvkv4fyu2p6szetxn4jookffvc4y45sq'
-url = 'https://dev.azure.com/tpwange/_apis/projects?api-version=6.1-preview.4'
+
+#projects
+projectList_url = 'https://dev.azure.com/tpwange/_apis/projects?api-version=6.1-preview.4'
+
+#buildListurl = "https://dev.azure.com/{organization}/{project}/_apis/build/builds?api-version=6.1-preview.6"
+buildList_url = "https://dev.azure.com/tpwange/poc/_apis/build/builds?api-version=6.1-preview.6"
+
+#buildturl = 'https://dev.azure.com/{organization}/{project}/_apis/build/builds/{buildId}/artifacts?api-version=6.1-preview.5
+build_url = "https://dev.azure.com/tpwange/poc/_apis/build/builds/91/artifacts?api-version=6.1-preview.5"
+
+#buildArtifactsListurl = https://dev.azure.com/{organization}/{project}/_apis/build/builds/{buildId}/artifacts?artifactName={artifactName}&api-version=6.1-preview.5"
+buildArtifactsList_url = "https://dev.azure.com/tpwange/tpwange/_apis/build/builds/91/artifacts?artifactName='Code Coverage Report_91'&api-version=6.1-preview.5"
 
 data = [
  {
- "op": "add",
- "path": "/fields/System.Title",
- "value": "Sample task"
+    "op": "add",
+    "path": "/fields/System.Title",
+    "value": "Sample task"
  }
 ]
 
-r = requests.get(url, json=data, 
+r = requests.get(projectList_url, json=data, 
     headers={'Content-Type': 'application/json-patch+json'},
     auth=('', personal_access_token))
 
-print(r.json())
+# Serializing json & write to file
+json_object = json.dumps(r.json(), indent = 2)
+open("export\\projects.json", "w").write(json_object)
+
+print("export to projects.json------------------------------------------")
+
+r = requests.get(buildList_url, json=data, 
+    headers={'Content-Type': 'application/json-patch+json'},
+    auth=('', personal_access_token))
+
+# Serializing json & write to file
+json_object = json.dumps(r.json(), indent = 2)
+open("export\\buildList.json", "w").write(json_object)
+
+print("export to buildList.json---------------------------------------------")
+
+r = requests.get(build_url, json=data, 
+    headers={'Content-Type': 'application/json-patch+json'},
+    auth=('', personal_access_token))
+
+# Serializing json & write to file
+json_object = json.dumps(r.json(), indent = 2)
+open("export\\build.json", "w").write(json_object)
+print("export to build.json---------------------------------------------")
+
+r = requests.get(buildArtifactsList_url, json=data, 
+    headers={'Content-Type': 'application/json-patch+json'},
+    auth=('', personal_access_token))
+
+# Serializing json & write to file
+json_object = json.dumps(r.json(), indent = 2)
+open("export\\buildArtifactsList.json", "w").write(json_object)
+print("export to buildArtifactsList.json---------------------------------------------")
