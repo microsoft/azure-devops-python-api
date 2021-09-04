b61507d526f8f023d70b345f309d5fb2a6a1c010
tpwange <tpwange@gmail.com>
2021-09-04 21:54:52 +0800

    20210904-2154

--- getAzureProject.py
+++ getAzureProject.py
@@ -6,16 +6,17 @@ import json
 personal_access_token = '42muadbwmvihps424suvtvkv4fyu2p6szetxn4jookffvc4y45sq'
 
 #projects
-projectList_url = 'https://dev.azure.com/tpwange/_apis/projects?api-version=6.1-preview.4'
+projectList_url = "https://dev.azure.com/{organization}/_apis/projects?api-version=6.1-preview.4"
+projectList_url = projectList_url.format(organization="tpwange")
 
-#buildListurl = "https://dev.azure.com/{organization}/{project}/_apis/build/builds?api-version=6.1-preview.6"
-buildList_url = "https://dev.azure.com/tpwange/poc/_apis/build/builds?api-version=6.1-preview.6"
+buildList_url = "https://dev.azure.com/{organization}/{project}/_apis/build/builds?api-version=6.1-preview.6"
+buildList_url = buildList_url.format(organization="tpwange", project="poc")
 
-#buildturl = 'https://dev.azure.com/{organization}/{project}/_apis/build/builds/{buildId}/artifacts?api-version=6.1-preview.5
-build_url = "https://dev.azure.com/tpwange/poc/_apis/build/builds/91/artifacts?api-version=6.1-preview.5"
+build_url = "https://dev.azure.com/{organization}/{project}/_apis/build/builds/{buildId}/artifacts?api-version=6.1-preview.5"
+build_url = build_url.format(organization="tpwange", project="poc", buildId=91)
 
-#buildArtifactsListurl = https://dev.azure.com/{organization}/{project}/_apis/build/builds/{buildId}/artifacts?artifactName={artifactName}&api-version=6.1-preview.5"
-buildArtifactsList_url = "https://dev.azure.com/tpwange/tpwange/_apis/build/builds/91/artifacts?artifactName='Code Coverage Report_91'&api-version=6.1-preview.5"
+buildArtifactsList_url = "https://dev.azure.com/{organization}/{project}/_apis/build/builds/{buildId}/artifacts?artifactName={artifactName}&api-version=6.1-preview.5"
+buildArtifactsList_url = buildArtifactsList_url.format(organization="tpwange", project="poc", buildId=91, artifactName="Code Coverage Report_91")
 
 data = [
  {
