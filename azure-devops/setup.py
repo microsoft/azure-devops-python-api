ce78fdd6e977927bc46b619493f395e97d9ee555
Anthony Shaw <anthony.p.shaw@gmail.com>
2022-10-26 15:52:29 +1100

    Update setup.py

--- azure-devops/setup.py
+++ azure-devops/setup.py
@@ -33,6 +33,7 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 3.8',
     'Programming Language :: Python :: 3.9',
     'Programming Language :: Python :: 3.10',
+    'Programming Language :: Python :: 3.11',
     'License :: OSI Approved :: MIT License',
 ]
 
