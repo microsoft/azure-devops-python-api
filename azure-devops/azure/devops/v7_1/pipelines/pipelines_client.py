c8348d5a0436fa0a35245d16be42f75f2cb742d7
PPRiphagen <patrick.riphagen@xsens.com>
2023-12-07 16:48:58 +0100

    Fixup, fix comments regarding version info

--- azure-devops/azure/devops/v7_1/pipelines/pipelines_client.py
+++ azure-devops/azure/devops/v7_1/pipelines/pipelines_client.py
@@ -184,7 +184,7 @@ class PipelinesClient(Client):
             Response for the list_pipelines method
 
             :param value:
-            :type value: :class:`<[Pipeline]> <azure.devops.v5_1.pipelines.models.[Pipeline]>`
+            :type value: :class:`<[Pipeline]> <azure.devops.v7_1.pipelines.models.[Pipeline]>`
             :param continuation_token: The continuation token to be used to get the next page of results.
             :type continuation_token: str
             """
